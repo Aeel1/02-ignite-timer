5bf4a2ee3dcf6f7b9ef5892c95f01ed68da6bbc1
AeeL1 <rafael.dod@hotmail.com>
2024-05-23 21:53:29 -0300

    Adicionando styled

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -16,9 +16,14 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 48px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-  ${props => {
+  background-color: ${props => props.theme['green-500']};
+  color: ${props => props.theme.white};
+  /*${props => {
     return css`
     background-color: ${buttonVariants[props.variant]}`
-  }}
+  }} */
 `
